da3a0cb8b789e0f4c034c39f8a3933a2b8a349aa
und3rdg <und3rdg@gmail.com>
2019-12-07 11:35:33 +0000

    update cfg, poe.sh flag for amdgpu

--- poe.sh
+++ poe.sh
@@ -2,7 +2,7 @@
 # WINEARCH=win32 WINEPREFIX=~/PoE wine  ~/PoE/drive_c/Program\ Files/Grinding\ Gear\ Games/Path\ of\ Exile/PathOfExile.exe --noasync
 cd ~/PoE/POE/
 
-if [ "$1" = "" ] ; then
+if [ "$1" = "nv" ] ; then
     export __GL_SHADER_DISK_CACHE_PATH=/home/undg/.local/share/Steam/steamapps/shadercache/301520/nvidiav1/
     export __GL_SHADER_DISK_CACHE_APP_NAME=steam_shader_cache
     export __GL_SHADER_DISK_CACHE_SKIP_CLEANUP=1
@@ -11,7 +11,17 @@ if [ "$1" = "" ] ; then
     echo "///////////////////////"
 fi
 
-wine64 PathOfExile_x64.exe \
+
+if [ "$1" = "f" ] ; then
+    export MESA_GLSL_CACHE_DIR=/home/undg/.local/share/Steam/steamapps/shadercache/255710  fossilize-replay /home/undg/.local/share/Steam/steamapps/shadercache/255710/fozpipelinesv4/steam_pipeline_cache.foz
+    echo "///////////////////"
+    echo "MESA_GLSL_CACHE_DIR"
+    echo "///////////////////"
+fi
+
+
+if [ "$1" = "" ] ; then
+    wine64 PathOfExile_x64.exe \
         --waitforpreload \
         --noasync \
         --nologo \
@@ -20,4 +30,6 @@ wine64 PathOfExile_x64.exe \
         # --garbage-generation 2 \
         # --nosound \
         # --softwareaudio \
+fi
+
 cd -
